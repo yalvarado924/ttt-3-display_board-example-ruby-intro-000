80b846c1838996f6b2eda9f6380ce2ba6ee8395b
jocular-firmware-2768 <yalvarado924@gmail.com>
2020-09-07 21:27:06 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,4 +5,4 @@ def display_board
   puts "-----------"# Define a method display_board that prints a 3x3 Tic Tac Toe Board
   puts "   ""|""   ""|""   "
 end
-print display_board
+print display_board# Define a method display_board that prints a 3x3 Tic Tac Toe Board
